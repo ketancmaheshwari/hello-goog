871e1e6400af09a59449469094cc2438a44f8647
ketancmaheshwari <ketancmaheshwari@gmail.com>
2013-12-02 11:39:38 -0500

    palindrome w logic

--- src/python/palindrome.py
+++ src/python/palindrome.py
@@ -8,19 +8,23 @@ import numpy as np
 
 #ToDo: use traditional logic
 def palindrome (name):
+    flag=True
     #find length of string
     slen = len(name)
+    i=0
+    j=slen-1
+    while i<=j:
+        if name[i]!=name[j]:
+            flag=False
+        i=i+1
+        j=j-1
 
-    #find middle
-    if slen % 2 == 0: # even
-        middle=slen/2
-    else:             # odd
-        middle=slen/2 + 1
-
-    #return name == name[::-1]
-    return middle
+    return flag
 
 
 if __name__=='__main__':
     #call the routine you'd like to run as main
-    print palindrome (sys.argv[1])
+    if palindrome (sys.argv[1]):
+        print sys.argv[1] + " is a palindrome"
+    else:
+        print sys.argv[1] + " is not a palindrome"

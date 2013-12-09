2b094a4e298051d930f58a1a99aebe5491b6ab26
ketancmaheshwari <ketancmaheshwari@gmail.com>
2013-12-09 16:42:38 -0500

    time it

---
+++ src/python/timeit.py
@@ -0,0 +1,18 @@
+#!/bin/env python
+
+#mainly for sys.argv[], sys.argv[0] is the name of the program
+import sys
+import time
+#mainly for arrays
+import numpy as np
+
+# Time the following methods
+
+def a():
+    print 'hello'
+
+def b():
+    print 'hi'
+
+if __name__=='__main__':
+    print 'hello'

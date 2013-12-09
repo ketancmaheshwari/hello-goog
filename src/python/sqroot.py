cf9a2ccc90374db2fc42885071a2432c9bc7aa66
ketancmaheshwari <ketancmaheshwari@gmail.com>
2013-12-09 13:51:47 -0600

    square root

---
+++ src/python/sqroot.py
@@ -0,0 +1,16 @@
+#!/bin/env python
+
+#mainly for sys.argv[], sys.argv[0] is the name of the program
+import sys
+#mainly for arrays
+import numpy as np
+
+def sqroot(anum):
+    """
+    Compute square root of a number via Newton's method
+    Newton's method is to approximate z = Sqrt(x) by picking a starting point z and then repeating:
+    z = z - (z*z - x)/(2*z)
+    """
+
+if __name__=='__main__':
+    print 'hello'

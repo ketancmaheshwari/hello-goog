57ae36c8f59344448b092de2291b1aa011766761
ketancmaheshwari <ketancmaheshwari@gmail.com>
2013-12-28 16:46:54 -0600

    _

---
+++ src/python/matmul.py
@@ -0,0 +1,9 @@
+#!/bin/env python
+
+#mainly for sys.argv[], sys.argv[0] is the name of the program
+import sys
+#mainly for arrays
+import numpy as np
+
+if __name__=='__main__':
+    print 'hello'
